56bbd9a1a95c3a64302afcf84520b9eae69be534
Yoshi <yoshi@rocal.dev>
2025-03-31 01:15:10 -0700

    Add BUILD_PROFILE to token keys to differenciate tokens for each environment

--- rocal_cli/src/token_manager.rs
+++ rocal_cli/src/token_manager.rs
@@ -7,19 +7,19 @@ pub struct TokenManager;
 #[allow(dead_code)]
 impl TokenManager {
     pub fn set_token(kind: Kind, token: &str) -> Result<(), Error> {
-        let entry = Entry::new(kind.as_str(), DEFAULT_KEY)?;
+        let entry = Entry::new(&kind.to_string(), DEFAULT_KEY)?;
         entry.set_password(token)?;
         Ok(())
     }
 
     pub fn get_token(kind: Kind) -> Result<String, Error> {
-        let entry = Entry::new(kind.as_str(), DEFAULT_KEY)?;
+        let entry = Entry::new(&kind.to_string(), DEFAULT_KEY)?;
         let token = entry.get_password()?;
         Ok(token)
     }
 
     pub fn delete_token(kind: Kind) -> Result<(), Error> {
-        let entry = Entry::new(kind.as_str(), DEFAULT_KEY)?;
+        let entry = Entry::new(&kind.to_string(), DEFAULT_KEY)?;
         entry.delete_credential()?;
         Ok(())
     }
@@ -31,10 +31,14 @@ pub enum Kind {
 }
 
 impl Kind {
-    pub fn as_str(&self) -> &str {
+    pub fn to_string(&self) -> String {
         match self {
-            Kind::RocalAccessToken => "rocal_access_token",
-            Kind::RocalRefreshToken => "rocal_refresh_token",
+            Kind::RocalAccessToken => {
+                format!("{}:rocal_access_token", env!("BUILD_PROFILE"))
+            }
+            Kind::RocalRefreshToken => {
+                format!("{}:rocal_refresh_token", env!("BUILD_PROFILE"))
+            }
         }
     }
 }
